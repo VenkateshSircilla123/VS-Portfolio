3fc9732e089c680c2f49de0fbfee5893c58abfd6
VenkateshSircilla123 <sircillavenkatesh0@gmail.com>
2024-08-31 22:52:28 +0530

    fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -31,6 +31,7 @@ const Computers = ({ isMobile }) => {
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
+  console.log(isMobile);
 
   useEffect(() => {
     // Add a listener for changes to the screen size
@@ -55,11 +56,12 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      className="hidden md:block"
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
